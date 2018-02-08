02909100b89810abaebd527faed78fa45521de7b
Jacob Marks <jacobjmarks@outlook.com>
2018-02-08 23:16:39 +1000

    Refined kmer counting

--- count.cpp
+++ count.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
-map <string, uint> counted_kmers;
-
-void count_kmer(const string * kmer_p) {
-    const string kmer = *kmer_p;
-    map<string, uint>::const_iterator it;
-    it = counted_kmers.find(kmer);
-    if (it != counted_kmers.end()) {
-        counted_kmers[kmer] += 1;
-    } else {
-        counted_kmers.emplace(kmer, 1);
-    }
-    counted_kmers.begin();
-}
+unordered_map <string, uint> counted_kmers;
 
 void output_kmer_counts() {
-    map<string, uint>::const_iterator it = counted_kmers.begin();
+    unordered_map<string, uint>::const_iterator it = counted_kmers.begin();
     for (; it != counted_kmers.end(); it++) {
         cout << it->first << " " << it->second << endl;
     }
@@ -57,7 +45,7 @@ int main(int argc, char* argv[]) {
                 kmer_buffer[i] += ch;
 
                 if (kmer_buffer[i].length() == KMER_LEN) {
-                    count_kmer(&kmer_buffer[i]);
+                    counted_kmers[kmer_buffer[i]]++;
                     kmer_buffer[i].clear();
                 }
             }
@@ -68,12 +56,11 @@ int main(int argc, char* argv[]) {
 
     file.close();
 
-    output_kmer_counts();
-
     chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
     chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-
     cerr << time_span.count() << 's' << endl;
+
+    output_kmer_counts();
 
     return 0;
 }
