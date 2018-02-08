e051f946fa2e25472327a39f9980021cc8c5c92e
Jacob Marks <jacobjmarks@outlook.com>
2018-02-08 22:36:47 +1000

    Setup basic kmer counting via map

--- count.cpp
+++ count.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <chrono>
+#include <map>
 
 using namespace std;
 
+map <string, uint> counted_kmers;
+
+void count_kmer(const string * kmer_p) {
+    const string kmer = *kmer_p;
+    map<string, uint>::const_iterator it;
+    it = counted_kmers.find(kmer);
+    if (it != counted_kmers.end()) {
+        counted_kmers[kmer] += 1;
+    } else {
+        counted_kmers.emplace(kmer, 1);
+    }
+    counted_kmers.begin();
+}
+
+void output_kmer_counts() {
+    map<string, uint>::const_iterator it = counted_kmers.begin();
+    for (; it != counted_kmers.end(); it++) {
+        cout << it->first << " " << it->second << endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
+    chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
     if (argc < 3) throw invalid_argument("Please specify filename and kmer length.");
 
     ifstream file;
@@ -15,7 +38,7 @@ int main(int argc, char* argv[]) {
     const uint KMER_LEN = atoi(argv[2]);
 
     string kmer_buffer[KMER_LEN];
-    int max_buffer_index = 1;
+    uint max_buffer_index = 1;
 
     char ch;
     while ((ch = file.get()) != EOF) {
@@ -34,7 +57,7 @@ int main(int argc, char* argv[]) {
                 kmer_buffer[i] += ch;
 
                 if (kmer_buffer[i].length() == KMER_LEN) {
-                    cout << kmer_buffer[i] << endl;
+                    count_kmer(&kmer_buffer[i]);
                     kmer_buffer[i].clear();
                 }
             }
@@ -44,6 +67,13 @@ int main(int argc, char* argv[]) {
     }
 
     file.close();
+
+    output_kmer_counts();
+
+    chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+    chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+
+    cerr << time_span.count() << 's' << endl;
 
     return 0;
 }
