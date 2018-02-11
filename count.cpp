ca4530e5c818707aa3e0ef1dfdb7fdf8efa81900
Jacob Marks <jacobjmarks@outlook.com>
2018-02-12 00:39:26 +1000

    Implemented some functional yet terrible concurrency with OpenMP
    Breaks canonical counting

--- count.cpp
+++ count.cpp
@@ -4,15 +4,16 @@
 #include <unordered_map>
 #include <vector>
 #include <iomanip>
+#include <omp.h>
 
 using namespace std;
 
-const bool COUNT_CANONICAL = true;
+// const bool COUNT_CANONICAL = false;
 
 const string ALPHABET = "ACTG";
 uint KMER_LEN;
 
-unordered_map <string, uint> counted_kmers;
+unordered_map <string, uint> counted_kmers[8];
 
 char complement(const char nucleotide) {
     switch (nucleotide) {
@@ -24,20 +25,28 @@ char complement(const char nucleotide) {
     }
 }
 
-void count_kmer(const string * kmer_p) {
-    if (COUNT_CANONICAL) {
-        string reverse_complement;
-        for (int i = KMER_LEN-1; i >= 0; i--) reverse_complement.push_back(complement((*kmer_p)[i]));
-        if (counted_kmers.find(reverse_complement) != counted_kmers.end()) {
-            counted_kmers[reverse_complement]++;
-            return;
-        }
-    }
-    counted_kmers[*kmer_p]++;
+void count_kmer(const string kmer) {
+    // if (COUNT_CANONICAL) {
+    //     string reverse_complement;
+    //     for (int i = KMER_LEN-1; i >= 0; i--) reverse_complement.push_back(complement(kmer[i]));
+    //     if (counted_kmers.find(reverse_complement) != counted_kmers.end()) {
+    //         counted_kmers[reverse_complement]++;
+    //         return;
+    //     }
+    // }
+
+    counted_kmers[omp_get_thread_num()][kmer]++;
 }
 
 void output_kmer_counts() {
-    for (const auto &keyval : counted_kmers) {
+    unordered_map <string, uint> count_totals;
+    for (int i = 0; i < 8; i++) {
+        for (auto &keyval : counted_kmers[i]) {
+            count_totals[keyval.first] += keyval.second;
+        }
+    }
+
+    for (const auto &keyval : count_totals) {
         cout << keyval.first << "\t" << keyval.second << endl;
     }
 }
@@ -57,26 +66,32 @@ int main(int argc, char* argv[]) {
     cerr << "Counting K-mers...";
     chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
 
-    char ch;
-    while ((ch = file.get()) != EOF) {
-        if (ch == '\n' || ch == '\r') {
-            // Skip
-        } else if (ALPHABET.find(ch) == string::npos) {
-            // Char is not part of nucleotide alphabet, break current kmers
-            if (ch == '>') file.ignore(UINT32_MAX, '\n');
-            max_buffer_index = 1;
-            for (uint i = 0; i < KMER_LEN; i++) kmer_buffer[i].clear();
-        } else {
-            for (uint i = 0; i < max_buffer_index; i++) {
-                kmer_buffer[i] += ch;
+    #pragma omp parallel num_threads(8)
+    #pragma omp single
+    {
+        char ch;
+        while ((ch = file.get()) != EOF) {
+            if (ch == '\n' || ch == '\r') {
+                // Skip
+            } else if (ALPHABET.find(ch) == string::npos) {
+                // Char is not part of nucleotide alphabet, break current kmers
+                if (ch == '>') file.ignore(UINT32_MAX, '\n');
+                max_buffer_index = 1;
+                for (uint i = 0; i < KMER_LEN; i++) kmer_buffer[i].clear();
+            } else {
+                for (uint i = 0; i < max_buffer_index; i++) {
+                    kmer_buffer[i] += ch;
 
-                if (kmer_buffer[i].length() == KMER_LEN) {
-                    count_kmer(&kmer_buffer[i]);
-                    kmer_buffer[i].clear();
+                    if (kmer_buffer[i].length() == KMER_LEN) {
+                        string kmer = kmer_buffer[i];
+                        #pragma omp task
+                        count_kmer(kmer);
+                        kmer_buffer[i].clear();
+                    }
                 }
-            }
 
-            if (max_buffer_index < KMER_LEN) max_buffer_index++;
+                if (max_buffer_index < KMER_LEN) max_buffer_index++;
+            }
         }
     }
 
